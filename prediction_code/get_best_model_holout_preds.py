8fc5a686f385f93308ed3e22d34402ac0f0169c0
Mitch Watts <{ID}+{username}@users.noreply.github.com>
2021-08-16 20:46:13 +0100

    final preds

--- prediction_code/get_best_model_holout_preds.py
+++ prediction_code/get_best_model_holout_preds.py
@@ -1,6 +1,8 @@
 import os, sys
 import gzip, pickle
 
+import pandas as pd
+
 CDIR = os.path.dirname(os.path.abspath(__file__))
 PPDIR = os.path.dirname(os.path.dirname(CDIR))
 sys.path.append(PPDIR) # rather than force you to add package to path in bash, 
@@ -17,6 +19,20 @@ def main():
 
     return overall_holdout_set_preds
 
+def fill_missing_preds_as_nan(overall_holdout_set_preds):
+    # NOTE: my predictions are missing some of the holdout set.
+    # to get them do this but fill with nan where missing
+    holdout = pd.read_csv("../data/raw_data/labels_predict.txt")
+    merged = pd.merge(holdout, overall_holdout_set_preds, on=["customerId", "productId"], how="outer")
+    final = merged[["customerId", "productId", "purchase_probability_y"]]
+    final.rename({"purchase_probability_y":"purchase_probability"}, axis=1, inplace=True)
+    return final
+
+
 if __name__ == "__main__":
     overall_holdout_set_preds = main()
-
+    missing_preds_as_nan = fill_missing_preds_as_nan(overall_holdout_set_preds)
+    breakpoint()
+    missing_preds_as_nan.to_csv("../data/predictions/holdout_predictions.txt", index=False) # save
+    
+    
